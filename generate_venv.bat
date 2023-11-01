344f86761f5fc58ac87a0b950054cc4ce4c2d37a
Marysia Tańska <marysiatanska@gmail.com>
2023-11-01 12:29:57 +0000

    typo

--- generate_venv.bat
+++ generate_venv.bat
@@ -1,4 +1,4 @@
-set VEV_PARENT_DIR=C:\coding_projects\cci_ual\python-exercises
+set VENV_PARENT_DIR=C:\coding_projects\cci_ual\python-exercises
 set VENV_NAME=python-venv
 set PYTHON_VERSION=3.9
 cd %VEV_PARENT_DIR%
